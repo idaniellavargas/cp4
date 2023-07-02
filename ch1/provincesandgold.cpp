319341eb72c9fd5b036ae801259b10d89e8283e4
Daniella Vargas <103683122+idaniellavargas@users.noreply.github.com>
2023-07-01 23:26:50 -0500

    Create provincesandgold.cpp

---
+++ ch1/provincesandgold.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    int g, s, c;
+    cin >> g >> s >> c;
+    g *= 3;
+    s *= 2;
+    int bp = g + s + c;
+    if (bp < 2)cout << "Copper" << endl;
+    else if (bp >= 8)cout << "Province or Gold" << endl;
+    else if (bp >= 6)cout << "Duchy or Gold" << endl;
+    else if (bp >= 5)cout << "Duchy or Silver" << endl;
+    else if (bp >= 3)cout << "Estate or Silver" << endl;
+    else if (bp >= 2)cout << "Estate or Copper" << endl;
+    return 0;
+}
