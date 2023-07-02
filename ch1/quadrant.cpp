a4c3b5de380ed8a118b1bf30e3091b972a9cb357
Daniella Vargas <103683122+idaniellavargas@users.noreply.github.com>
2023-07-01 23:31:04 -0500

    Create quadrant.cpp

---
+++ ch1/quadrant.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if (x > 0 && y > 0)cout << 1;
+    else if (x < 0 && y < 0)cout << 3;
+    else if (x > 0 && y < 0)cout << 4;
+    else cout << 2;
+    cout << endl;
+
+    return 0;
+}
